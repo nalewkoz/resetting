9d97ef16c41e0d99366c46912771544922a61e62
nalewkoz <nalewkoz@gmail.com>
2015-04-04 17:27:22 +0200

    New plot and file generating data for 3d plots added

---
+++ mfat3d.m
@@ -0,0 +1,20 @@
+function T=mfat3d(x)
+	alphap=1.6:0.02:2;
+	r=0.05:0.05:10;
+	Na=length(alphap);
+	Nr=length(r);
+	T=zeros(Na,Nr);
+	for i=1:Na
+		a=alphap(i)
+		D=(pi/(2*gamma(1-1/a)))^a;
+		for j=1:Nr
+			T(i,j)=mfat_anal_final(r(j),D,x,a);
+		end
+	end
+	min(min(T))
+	max(max(T))
+
+	%h=surf(r,alpha,T);
+	%printpdf(h,sprintf('mfat3d_%.1f',x));
+	save(sprintf('data3dplots_%.1f.mat',x),'x','alphap','r','T');
+end
