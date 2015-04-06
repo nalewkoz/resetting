aebe2c6e640bcd31ab2e6cf1395d3ae35c0a30d2
nalewkoz <nalewkoz@gmail.com>
2015-04-06 15:30:14 +0200

    Przeniesione ploty dla porzadku, zmiana kolorow

--- plot3d.m
+++ plot3d.m
@@ -1,9 +1,11 @@
 function [h, fname]=plot3d(xx)
-    load(sprintf('data3dplots_%.1f.mat',xx));
+    load(sprintf('data3dplot/data3dplots_%.1f.mat',xx));
     h=surf(r,alphap,T);
     h
     ylabel('\alpha');
     xlabel('r');
+    zlabel('<T>');
+    colormap(bone);
     fname=sprintf('mfat3d_%.1f',x);
     %printpdf(h,fname);
 end
