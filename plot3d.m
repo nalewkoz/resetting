6c12fec054e459770e8baca8b1295454196b9ae9
nalewkoz <nalewkoz@gmail.com>
2015-04-06 23:03:17 +0200

    Wykresy w PDFach gotowe

--- plot3d.m
+++ plot3d.m
@@ -1,11 +1,15 @@
 function [h, fname]=plot3d(xx)
-    load(sprintf('data3dplot/data3dplots_%.1f.mat',xx));
-    h=surf(r,alphap,T);
-    h
-    ylabel('\alpha');
-    xlabel('r');
-    zlabel('<T>');
+    fs_label=14;
+    
+    h=figure(1);
+    load(sprintf('ploty3d/data3dplots_%.1f.mat',xx));
+    surfc(r,alphap,T);%,'EdgeColor','none');
     colormap(bone);
+    rotate3d on
+    %shading interp
+    ylabel('\alpha','fontsize',fs_label);
+    xlabel('r','fontsize',fs_label);
+    zlabel('<T>','fontsize',fs_label);
     fname=sprintf('mfat3d_%.1f',x);
     %printpdf(h,fname);
 end
---
+++ plot3d_allinone.m
@@ -0,0 +1,30 @@
+function [h, fname]=plot3d_allinone(xx)
+    fs_label=14;
+    
+    h=figure(1);
+    
+    a_ticks=[1.7 1.85 2;1.6 1.8 2;1.5 1.75 2;1.4 1.7 2];
+    r_ticks=[2 5 8; 0.02 0.06 0.1; 0.01 0.02 0.03; 0.5*10^(-3) 1*10^(-3) 1.5*10^(-3)];
+    
+    for i=1:length(xx)
+        load(sprintf('ploty3d/data3dplots_%.1f.mat',xx(i)));
+        subplot(2,2,i);
+        surfc(r,alphap,T);%,'EdgeColor','none');
+        colormap(bone);
+        %shading interp
+        axis([min(r) max(r) min(alphap) max(alphap)]);
+        
+        set(gca,'XTick',r_ticks(i,:))
+        set(gca,'YTick',a_ticks(i,:))
+      
+        ylabel('\alpha','fontsize',fs_label);
+        xlabel('r','fontsize',fs_label);
+        zlabel('<T>','fontsize',fs_label);
+        
+        title(sprintf('x=%.0f',xx(i)));
+    end
+    
+    
+    fname='plot3Dallinone';
+    printpdf(h,fname);
+end
