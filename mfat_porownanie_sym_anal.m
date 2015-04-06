6fed14f2640569c3e3d80e08a8c0412dca131e12
nalewkoz <nalewkoz@gmail.com>
2015-04-06 23:42:55 +0200

    Zmiana struktury i dodanie porownania anal/sym

---
+++ mfat_porownanie_sym_anal.m
@@ -0,0 +1,40 @@
+dt=0.01;
+eps=0.01;
+M=10^3;
+
+alpha_tab=[2 1.8 1.6];
+x0=1;
+r_tab=0.5:0.5:5;
+
+meanTs=zeros(length(alpha_tab),length(r_tab));
+disp('Pierwsza petla (symulacje)...');
+tic
+for i=1:length(alpha_tab)
+	
+	a=alpha_tab(i);
+	lambda=2*gamma(1-1/a)/pi;
+	D=1/lambda^a;
+	for j=1:length(r_tab)
+		fprintf('\ralpha=%.1f r=%.2f',a,r_tab(j));
+		meanTs(i,j)=mfat_sym_alpha(D,x0,r_tab(j),a,dt,eps,M);
+	end
+end
+fprintf('\n');
+toc
+r_tab2=1:0.1:5;
+meanTa=zeros(length(alpha_tab),length(r_tab));
+disp('Druga petla (calkowanie wzoru analitycznego)...');
+tic
+for i=1:length(alpha_tab)
+	a=alpha_tab(i);
+        lambda=2*gamma(1-1/a)/pi;
+        D=1/lambda^a;
+	for j=1:length(r_tab2)
+		fprintf('\ralpha=%.1f r=%.2f',a,r_tab2(j));
+		meanTa(i,j)=mfat_anal_final(r_tab2(j),D,x0,a);
+	end
+end
+fprintf('\n');
+toc
+save('data4comparison.mat','meanTs','meanTa','r_tab','r_tab2','alpha_tab');
+
--- mfat_sym_alpha.m
+++ mfat_sym_alpha.m
@@ -10,14 +10,14 @@ function meanT=mfat_sym_alpha(D,x0,r,alpha,dt,eps,M)
     
     t_akt=0;
     i=1;
-    tic
+
     while(Nover<M)
         i=i+1;
         t_akt=t_akt+dt;
         
         K=M-Nover;
         over_old(1:K)=over(1:K);
-        dX(1:K,1)=wsp*rand_alpha(M-Nover,alpha,0,1,0);
+        dX(1:K,1)=wsp*rand_alpha2(M-Nover,1,alpha,1);
         
         X(1:K,1)=X(1:K,1)+dX(1:K);     % Aktualizujemy tylko te wektory, dla ktorych nie przekroczymy lewej granicy i nie przekroczylismy -xm+r (wowczas over)
         czy_reset(1:K)=rand(K,1)<r*dt;
@@ -38,7 +38,7 @@ function meanT=mfat_sym_alpha(D,x0,r,alpha,dt,eps,M)
 %         if(wskaznik<=K)        wskaznik=I(wskaznik); end %Nie dzial [Dzia³a tylko gdy M=1]
 %         traj(i)=X(wskaznik,1);
     end
-    toc
+  
 %     plot(czasy,traj);
     meanT=mean(T);
 end
