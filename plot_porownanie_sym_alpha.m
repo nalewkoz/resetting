49f1b1a1ff40da5f17f226bdabc3cb31842d62c2
nalewkoz <nalewkoz@gmail.com>
2015-04-07 16:09:14 +0200

    ..

--- plot_porownanie_sym_alpha.m
+++ plot_porownanie_sym_alpha.m
@@ -2,7 +2,7 @@ load('data/data4comparison.mat');
 n=1;
 h=figure
 hold on
-plot(r_tab2,meanTa(:,:),'r');
+plot(r_tab2,log(meanTa(:,:)),'r');
 %plot(r_tab,meanTs(:,:),'b.','MarkerSize',12);
 
 for i=1:size(meanTs,1)
@@ -12,6 +12,7 @@ set(gca,'XTick',r_tab)
 
 xlabel('r','fontsize',fs_label);
 ylabel('log <T>','fontsize',fs_label);
+
 
 
 printpdf(h,'graphics/mfat_comp_r');
