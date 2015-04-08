931c099c74a7fc074ee3d317a7e7d204a7a29a37
nalewkoz <nalewkoz@gmail.com>
2015-04-08 19:28:15 +0200

    From now on we plot two comparisons together

--- plot_porownanie_sym_alpha.m
+++ plot_porownanie_sym_alpha.m
@@ -1,9 +1,13 @@
+% Pierwszy rysunek
+
 load('data/data4comparison.mat');
 n=1;
 h=figure
+
+subplot(1,2,2);
 hold on
-plot(r_tab2,log(meanTa(:,:)),'k');
-plot(r_tab,meanTs(:,:),'k.','MarkerSize',12);
+plot(r_tab2,log(meanTa(:,:)),'b');
+plot(r_tab,log(meanTs(:,:)),'k.','MarkerSize',12);
 
 %for i=1:size(meanTs,1)
 %    errorbar(r_tab,log(meanTs(i,:)),3*sdevT(i,:)./meanTs(i,:),'k.');
@@ -13,7 +17,25 @@ set(gca,'XTick',r_tab)
 xlabel('r','fontsize',fs_label);
 ylabel('log <T>','fontsize',fs_label);
 
-legend('Analytical formula','Monte Carlo');
+%legend('Analytical formula','Monte Carlo');
+
+% Drugi rysunek
+fs_label=14;
+
+load('data/data4comparison3.mat');
+subplot(1,2,1);
+hold on
+plot(x_tab2,log(meanTa(:,:)),'b');
+%plot(x_tab,log(meanTs(:,:)),'k.','MarkerSize',12);
+
+for i=1:size(meanTs,1)
+    errorbar(x_tab,log(meanTs(i,:)),3*sdevT(i,:)./meanTs(i,:),'k.');
+end
+set(gca,'XTick',x_tab)
+
+xlabel('x','fontsize',fs_label);
+ylabel('log <T>','fontsize',fs_label);
+grid on
+axis tight
 
 printpdf(h,'graphics/mfat_comp_r');
-
