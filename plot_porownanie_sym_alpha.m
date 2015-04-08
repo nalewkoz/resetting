e993407d84475ace968e2924ad08eefefa5d618a
nalewkoz <nalewkoz@gmail.com>
2015-04-08 19:15:54 +0200

    ..

--- plot_porownanie_sym_alpha.m
+++ plot_porownanie_sym_alpha.m
@@ -2,18 +2,18 @@ load('data/data4comparison.mat');
 n=1;
 h=figure
 hold on
-plot(r_tab2,log(meanTa(:,:)),'r');
-%plot(r_tab,meanTs(:,:),'b.','MarkerSize',12);
+plot(r_tab2,log(meanTa(:,:)),'k');
+plot(r_tab,meanTs(:,:),'k.','MarkerSize',12);
 
-for i=1:size(meanTs,1)
-    errorbar(r_tab,log(meanTs(i,:)),3*sdevT(i,:)./meanTs(i,:),'k.');
-end
+%for i=1:size(meanTs,1)
+%    errorbar(r_tab,log(meanTs(i,:)),3*sdevT(i,:)./meanTs(i,:),'k.');
+%end
 set(gca,'XTick',r_tab)
 
 xlabel('r','fontsize',fs_label);
 ylabel('log <T>','fontsize',fs_label);
 
-
+legend('Analytical formula','Monte Carlo');
 
 printpdf(h,'graphics/mfat_comp_r');
 
