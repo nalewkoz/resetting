80bc635731ebbf0ee23d1fe68e33210276eacae1
nalewkoz <nalewkoz@gmail.com>
2015-04-09 01:47:37 +0200

    mfpt

--- mfpt_sym_subdiff.m
+++ mfpt_sym_subdiff.m
@@ -28,7 +28,7 @@ function [meanT sdevT]=mfpt_sym_subdiff(D,x0,r,alpha,mu,dt,M)
 %         czy_przeszlo(1:K)=(rand(K,1)<exp(-( (X(1:K,1)-eps).*(X(1:K,1)-dX(1:K,1)-eps)/(D*dt) ) ))&(1-czy_reset(1:K));
 %         X(1:K,1)=X(1:K,1).*(1-czy_przeszlo(1:K))+czy_przeszlo(1:K)*(0);
         
-        [X(K:-1:1,:) I]=sortrows(X(1:K,:),2);
+        [X(K:-1:1,:) I]=sortrows(X(1:K,:),1);
         over(1:K)=X(1:K)<0;
         Nover=Nover+sum(over(1:K));
         T(1:K)=T(1:K)+( (over(1:K)-over_old(1:K))>0 )*(t_akt-dt/2); %Wa¿ne: bierzemy czas do statystyki FPT w po³owie przedzia³u ca³kowania!
