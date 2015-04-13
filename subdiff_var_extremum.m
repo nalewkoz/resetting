c76041d864f7e613dd0512192a7c54bde8f4f400
nalewkoz <nalewkoz@gmail.com>
2015-04-13 13:39:58 +0200

    ..

--- subdiff_var_extremum.m
+++ subdiff_var_extremum.m
@@ -4,13 +4,17 @@ function y=subdiff_var_extremum(mu,dx,xmax)
 	fv(1)=fv(2);
 	ifv=cumtrapz(fv)*dx;
 
+	dfv=fv-ifv;
+	dfv=dfv(1:end-1).*dfv(2:end);
+	dfv=find(dfv<0);
+	x0=xciag(dfv);
+
 	h=figure;
-	hold on
-	plot(xciag,fv,'r');
-	plot(xciag,ifv,'k');
-	ifv(2)
-	fv(2)
-	printpdf(h,sprintf('subdiff_max%.0f.pdf',10*mu));
+        hold on
+        plot(xciag,fv,'r');
+        plot(xciag,ifv,'k');
+	plot(x0,fv(dfv),'rx','MarkerSize',10);
+        printpdf(h,sprintf('graphics/subdiff_max%.0f.pdf',10*mu));
 
 end
 
