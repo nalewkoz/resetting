e9f178138d1c256261f62451a554a8ee0326884c
nalewkoz <nalewkoz@gmail.com>
2015-04-13 13:28:16 +0200

    ..

--- subdiff_var_extremum.m
+++ subdiff_var_extremum.m
@@ -1,13 +1,15 @@
 function y=subdiff_var_extremum(mu,dx,xmax)
 	xciag=0:dx:xmax;
 	fv=f(xciag,mu);
+	fv(1)=fv(2);
 	ifv=cumtrapz(fv)*dx;
 
 	h=figure;
 	hold on
 	plot(xciag,fv,'r');
 	plot(xciag,ifv,'k');
-	
+	ifv(2)
+	fv(2)
 	printpdf(h,sprintf('subdiff_max%.0f.pdf',10*mu));
 
 end
