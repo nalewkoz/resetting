ab13c588a3c8d6a0ccd20917be03aabc4aeec88c
nalewkoz <nalewkoz@gmail.com>
2015-04-14 12:40:52 +0200

    ..

--- subdiff_var_extremum.m
+++ subdiff_var_extremum.m
@@ -9,24 +9,24 @@ function y=subdiff_var_extremum(mu,dx,xmax)
 	dfv=dfv(1:end-1).*dfv(2:end);
 	dfv=find(dfv<0);
 	x0=xciag(dfv)
-
+	
+	fvhyper=xciag.^mu.*(1/mu-1+hypergeom([],[1+mu],xciag));
 	h=figure;
         hold on
         plot(xciag,log(fv),'r');
         plot(xciag,log(ifv),'k');
-
+	plot(xciag,log(fvhyper));
 	plot(x0,log(fv(dfv)),'rx','MarkerSize',10);
         printpdf(h,sprintf('graphics/subdiff_max%.0f.pdf',10*mu));
 
-	fv1=hypergeom([],[1+mu],xciag)+1/mu-1;
-	%fv1=fv1-(2-mu)/(1-mu);
-        fv1=fv1.*xciag.*exp(-xciag)-1;
-	fv2=fv1(1:end-1).*fv1(2:end);
-        fv2=find(fv2<0);
-        x02=xciag(fv2)
-	h=figure
-	plot(xciag, fv1,'.r');
-	printpdf(h,sprintf('graphics/subdiff_max2%.0f.pdf',10*mu));
+%	fv1=hypergeom([],[1+mu],xciag)+1/mu-1;
+%        fv1=fv1.*xciag.*exp(-xciag)-1;
+%	fv2=fv1(1:end-1).*fv1(2:end);
+ %       fv2=find(fv2<0);
+ %       x02=xciag(fv2)
+%	h=figure
+%	plot(xciag, fv1,'.r');
+%	printpdf(h,sprintf('graphics/subdiff_max2%.0f.pdf',10*mu));
 end
 
 function y=f(x,mu)
