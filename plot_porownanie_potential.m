6045c1c3d1de8fa1c3eff0d8290990da8b74ba35
nalewkoz <nalewkoz@gmail.com>
2015-05-21 21:27:00 +0200

    Zmiany w porownaniu na laptopie
    Te zmiany powinny byc zachowane, nie te z pantery!

--- plot_porownanie_potential.m
+++ plot_porownanie_potential.m
@@ -1,12 +1,16 @@
 fs_label=14;
 h=figure;
 hold on;
-znaczki=['ro';'g+'];
-wielkosci=[6;6];
-load('data/opt_potential_delta.mat');
-errorbar(ctab,Ts,3*sdTs,znaczki(2,:),'MarkerSize',wielkosci(2));
-load('data/opt_potential_exp.mat');
-errorbar(ctab,Ts/4,3*sdTs/4,znaczki(1,:),'MarkerSize',wielkosci(1));
+znaczki=['ko';'k+'];
+wielkosci=[8;8];
+load('data/opt_potential_delta2.mat');
+M
+%errorbar(ctab,Ts,3*sdTs,znaczki(2,:),'MarkerSize',wielkosci(2));
+plot(ctab,Ts,znaczki(2,:),'MarkerSize',wielkosci(2));
+load('data/opt_potential_exp2.mat');
+%errorbar(ctab,Ts/4,3*sdTs/4,znaczki(1,:),'MarkerSize',wielkosci(1));
+plot(ctab,Ts/4,znaczki(1,:),'MarkerSize',wielkosci(1));
+M
 
 z=min(ctab):0.01:max(ctab);
 plot(z,(z+exp(z)+2*exp(-z)-3)./z.^2,'k');
@@ -14,6 +18,6 @@ plot(z,(z+exp(z)+2*exp(-z)-3)./z.^2,'k');
 xlabel('z','fontsize',fs_label);
 ylabel('MFPT','fontsize',fs_label);
 
-legend('Monte Carlo: x_t=\pm1','Monte Carlo (rescaled): x_t Laplace-distributed','Analytical prediction');
+legend('MC: x_t=\pm1','MC: x_t Laplace-distributed','Analytical prediction');
 
 printpdf(h,'graphics/potential_comp');
