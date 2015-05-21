95cd1a448fd62b12337dc546c24718692b044547
nalewkoz <nalewkoz@gmail.com>
2015-05-21 21:16:46 +0200

    ..

--- plot_porownanie_potential.m
+++ plot_porownanie_potential.m
@@ -3,10 +3,10 @@ h=figure;
 hold on;
 znaczki=['ro';'g+'];
 wielkosci=[6;6];
+load('data/opt_potential_exp2.mat');
+errorbar(ctab,Ts/4,3*sdTs/4,znaczki(1,:),'MarkerSize',wielkosci(1));
 load('data/opt_potential_delta2.mat');
 errorbar(ctab,Ts,3*sdTs,znaczki(2,:),'MarkerSize',wielkosci(2));
-load('data/opt_potential_exp.mat');
-errorbar(ctab,Ts/4,3*sdTs/4,znaczki(1,:),'MarkerSize',wielkosci(1));
 
 z=min(ctab):0.01:max(ctab);
 plot(z,(z+exp(z)+2*exp(-z)-3)./z.^2,'k');
