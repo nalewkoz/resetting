3b7f7b35e3bcea5f47daee41198c45a194e5dea0
nalewkoz <nalewkoz@gmail.com>
2015-05-21 21:32:06 +0200

    Usunieto zbedne kawalki po mergu

--- plot_porownanie_potential.m
+++ plot_porownanie_potential.m
@@ -1,7 +1,7 @@
 fs_label=14;
 h=figure;
 hold on;
-<<<<<<< HEAD
+
 znaczki=['ko';'k+'];
 wielkosci=[8;8];
 load('data/opt_potential_delta2.mat');
@@ -12,14 +12,6 @@ load('data/opt_potential_exp2.mat');
 %errorbar(ctab,Ts/4,3*sdTs/4,znaczki(1,:),'MarkerSize',wielkosci(1));
 plot(ctab,Ts/4,znaczki(1,:),'MarkerSize',wielkosci(1));
 M
-=======
-znaczki=['ro';'g+'];
-wielkosci=[6;6];
-load('data/opt_potential_exp2.mat');
-errorbar(ctab,Ts/4,3*sdTs/4,znaczki(1,:),'MarkerSize',wielkosci(1));
-load('data/opt_potential_delta2.mat');
-errorbar(ctab,Ts,3*sdTs,znaczki(2,:),'MarkerSize',wielkosci(2));
->>>>>>> origin/master
 
 z=min(ctab):0.01:max(ctab);
 plot(z,(z+exp(z)+2*exp(-z)-3)./z.^2,'k');
@@ -29,4 +21,4 @@ ylabel('MFPT','fontsize',fs_label);
 
 legend('MC: x_t=\pm1','MC: x_t Laplace-distributed','Analytical prediction');
 
-printpdf(h,'graphics/potential_comp2');
+printpdf(h,'graphics/potential_comp');
