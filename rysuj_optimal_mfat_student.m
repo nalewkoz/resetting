b0e172634ce13e54e25b8ebc97fea1c92f995d93
nalewkoz <nalewkoz@gmail.com>
2015-08-01 13:57:36 +0200

    blah

--- rysuj_optimal_mfat_student.m
+++ rysuj_optimal_mfat_student.m
@@ -1,6 +1,6 @@
 load('data_min_mfat_anal_student_fair_huge2');
 
-x=[0.001:0.001:0.02, 0.025:0.01:1, 1:0.1:100] ;
+x=[0.00001, 0.0001, 0.001:0.001:0.024, 0.025:0.01:1, 1:0.1:100] ;
 Na=length(alpha_tab);
 Nx=length(x);
 Tmin_matrix=zeros(Na,Nx);
