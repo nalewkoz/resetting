8f594f4c591b65ce22e9d69be74c314ad62712eb
Łukasz Kuśmierz <nalewkoz@gmail.com>
2015-08-09 23:22:56 +0200

    Poprawiony blad dla rysunku z x (zle miejsce krzyzyka)

--- rysuj_optimal_mfat_lambda.m
+++ rysuj_optimal_mfat_lambda.m
@@ -15,28 +15,28 @@ for i=1:Nx
     alpha_glopt(i)=alpha_tab(I(i));
 end
 
-h1=figure
-plot(x,T_glopt,'.');
-ylabel('T^*','fontsize',14);
-xlabel('\lambda_t','fontsize',14);
-grid on
-h2=figure
-hold on
-plot(log(x),log(r_glopt),'.');
-%plot(log(10.77),log(r_glopt(find(x==10.77))),'r+','MarkerSize',20);
-plot(log(3.25),log(r_glopt(find(x==3.25))),'r+','MarkerSize',20);
-ylabel('log(r^*)','fontsize',14);
-xlabel('log(\lambda_t)','fontsize',14);
-set(gca,'XTick',[-5 -3 -1 1 3 5])
-grid on
-h3=figure
-plot(x,alpha_glopt,'.');
-ylabel('\alpha^*','fontsize',14);
-xlabel('\lambda_t','fontsize',14);
-grid on
-printpdf(h1,'graphics/mfat_fair_lambda_T');
-printpdf(h2,'graphics/mfat_fair_lambda_r');
-printpdf(h3,'graphics/mfat_fair_lambda_alpha');
+%h1=figure
+%plot(x,T_glopt,'.');
+%ylabel('T^*','fontsize',14);
+%xlabel('\lambda_t','fontsize',14);
+%grid on
+%h2=figure
+%hold on
+%plot(log(x),log(r_glopt),'.');
+%%plot(log(10.77),log(r_glopt(find(x==10.77))),'r+','MarkerSize',20);
+%plot(log(3.25),log(r_glopt(find(x==3.25))),'r+','MarkerSize',20);
+%ylabel('log(r^*)','fontsize',14);
+%xlabel('log(\lambda_t)','fontsize',14);
+%set(gca,'XTick',[-5 -3 -1 1 3 5])
+%grid on
+%h3=figure
+%plot(x,alpha_glopt,'.');
+%ylabel('\alpha^*','fontsize',14);
+%xlabel('\lambda_t','fontsize',14);
+%grid on
+%printpdf(h1,'graphics/mfat_fair_lambda_T');
+%printpdf(h2,'graphics/mfat_fair_lambda_r');
+%printpdf(h3,'graphics/mfat_fair_lambda_alpha');
 
 h=figure;
 subplot(2,2,1);
