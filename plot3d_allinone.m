244f45622fcf91b93cae681840114b5ab4590f19
Łukasz Kuśmierz <nalewkoz@gmail.com>
2015-10-29 18:18:08 +0100

    Modyfikacja rysunkow...

--- plot3d_allinone.m
+++ plot3d_allinone.m
@@ -5,7 +5,7 @@ function [h, fname]=plot3d_allinone(xx)
     
     a_ticks=[1.7 1.85 2;1.6 1.8 2;1.5 1.75 2;1.4 1.7 2];
     r_ticks=[2 5 8; 0.02 0.06 0.1; 0.01 0.02 0.03; 0.5*10^(-3) 1*10^(-3) 1.5*10^(-3)];
-    
+    tit=['a','b','c','d'];
     for i=1:length(xx)
         load(sprintf('graphics/ploty3d/data3dplots_%.1f.mat',xx(i)));
         subplot(2,2,i);
@@ -21,7 +21,8 @@ function [h, fname]=plot3d_allinone(xx)
         xlabel('r','fontsize',fs_label);
         zlabel('<T>','fontsize',fs_label);
         
-        title(sprintf('x=%.0f',xx(i)));
+        %title(sprintf('x=%.0f',xx(i)));
+        title(sprintf('(%s)',tit(i)),'fontsize',10);
     end
     
     
